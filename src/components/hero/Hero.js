a8d58a32d8f169ba8c8c2daa7f0829372cf29109
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-07-10 23:09:21 -0700

    styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,46 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Fade from 'react-reveal/Fade';
-import './Hero.css'
-
-import BgVideo from '../../assets/Bg-Video-2.mp4'
+import React from "react";
+import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
+import "./Hero.css";
+import Hero1 from '../../assets/Hero1.jpg'
+import Hero2 from '../../assets/Hero2.jpg'
+import Hero3 from '../../assets/Hero3.jpg'
+import Hero4 from '../../assets/Hero4.jpg'
 
 
 const Hero = () => {
-    return (
-<>        
+  return (
+    <>
+      <div className="hero">
 
-        <div className='hero'>
-            <video src={BgVideo} autoPlay muted loop playsInline className='video-bg' />
+      <div class="fling-minislide">
+        <img src={Hero1} alt="Slide 1" />
+        <img src={Hero2} alt="Slide 2" />
+        <img src={Hero3} alt="Slide 3" />
+        <img src={Hero4} alt="Slide 4" />
+      </div>
 
-
-
-            <div className="hero-container">
+      <div className="hero-container">
 
                 <div className="content">
-                <Fade left>
-                        <h2 className='subtitle'>Substance Abuse Addiction and Mental Health Treatment Facility</h2>
-                    </Fade>
+               <Fade left>
+                         <h2 className='subtitle'>Substance Abuse Addiction and Mental Health Treatment Facility</h2>
+                     </Fade>
                         <h1>“A great life starts with a strong mind and a strong body."</h1>
 
                         <br />
-                        <div className='hero-btn'>
-                        <Link to="/contact">
-                        <button>Contact Us</button>
-                    </Link>
-                    </div>
-                    <br />
-                    <Fade bottom>
-                        <p>Find your happiness and learn to put it first.</p>
-                    </Fade>
-                </div>
+                       <div className='hero-btn'>
+                         <Link to="/contact">
+                    <button>Contact Us</button>
+                    </Link>                     </div>
+                     <br />
+                     <Fade bottom>
+                         <p>Find your happiness and learn to put it first.</p>
+                     </Fade>
+                 </div>
             </div>
-        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Hero;
 
 
-</>
 
-    )
-}
 
-export default Hero
+
+
+
